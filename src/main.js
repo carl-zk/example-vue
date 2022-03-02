e746f711caea9ab8d53d0afe281107ac8cebe9f1
Carl <zxfspace@163.com>
2022-03-02 23:09:29 +0800

    create app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from "vue";
-import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp({
+  data() {
+    return {
+      count: 0,
+    };
+  },
+});
+
+app.mount("#app");
