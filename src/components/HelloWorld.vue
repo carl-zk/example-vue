361e661ba3c238ca8b60cffbcb7b4e9514e4def9
Carl <zxfspace@163.com>
2022-03-03 20:44:13 +0800

    start success

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,40 @@
 <script setup>
+import { ref } from 'vue'
+
 defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-});
+  msg: String
+})
+
+const count = ref(0)
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-    </h3>
-  </div>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+a {
+  color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
 import { createApp } from "vue";
+import App from "./App.vue";
 
-const app = createApp({
-  data() {
-    return {
-      count: 0,
-    };
-  },
-});
-
-app.mount("#app");
+createApp(App).mount("#app");
